bf35316d98e85c4e9aaaed82815a974777045edf
Isaac <devisaacalejandro@gmail.com>
2021-05-16 23:19:58 -0400

    commit tree 967b8df7dd28228fdc94390abce8eef03873b62f

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+
+
+  function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
+
+
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+
+//   return gifts;
+// }
+
+
+
+// gifts = ["teddy bear", "drone", "doll"];
+// writeCards(["Ada", "Brendan", "Ali"], "birthday"); 
